c729ef859fb9e1c6f219e962eba21ee324f70903
DanaDos <guldana.khalel@gmail.com>
2021-08-11 17:51:55 -0500

    third commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -1,5 +1,12 @@
 package package1;
 
 public class ClassOne {
+	
+	public static void main(String) [] args){
+		System.out.println("Hello world")
+		System.out.println("Welcom");
+		System.out.println("here is my third commit");
+	}
+
 
 }
